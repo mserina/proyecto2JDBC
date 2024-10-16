d15f1b9b621d32f253c3c28a8c89afc0d653b74e
DESKTOP-1CSI9JF\Alumno <dragontero2@gmail.com>
2024-10-16 14:37:23 +0200

    no message

--- ejercicioProyecto2/src/servicios/ConsultaSQLInterfaz.java
+++ ejercicioProyecto2/src/servicios/ConsultaSQLInterfaz.java
@@ -1,6 +1,7 @@
 package servicios;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 
 import dtos.ClubDto;
 import dtos.UsuarioDto;
@@ -10,5 +11,7 @@ public interface ConsultaSQLInterfaz {
 	public void añadirUsuarioBD(Connection conexionGenerada, UsuarioDto nuevoUsuario );
 	
 	public void añadirClubBD(Connection conexionGenerada, ClubDto nuevoClub );
+	
+	public ResultSet listaBaseDato();
 	
 }
